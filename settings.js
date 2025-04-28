c3d6dd8248e0e5fc39928870f39dd156d94d2341
mcrans <ransbaffoeameyaw7@gmail.com>
2025-04-27 17:07:05 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'McRans', // Your name
-  ownerNumber: '0593064505', //Your number
+  ownerNumber: '233593064505', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
